d38a364da2b28df274e960bf8521126027151aba
sajin3317 <vicentesajin@gmail.com>
2025-06-08 21:25:44 +0800

    Index Update

--- pages/index.vue
+++ pages/index.vue
@@ -4,26 +4,15 @@ import { toValue } from 'vue';
 </script>
 
 <template>
-<<<<<<< Updated upstream
-  <div class="flex items-center justify-center h-screen bg-white text-center">
-    <div>
-      <h1 class="text-5xl font-bold mb-4">Welcome to ClinicPOS</h1>
+
+   <div class="max-w-6xl mx-auto bg-sky-100 p-4 rounded-xl">
+    <h1 class="text-5xl font-bold mb-4">Welcome to ClinicPOS</h1>
       <p class="text-lg mb-8 text-gray-600">Your reliable clinic management system</p>
-      <div class="flex justify-center space-x-4">
+<div class="flex justify-center space-x-4">
         <NuxtLink to="/login" class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg">Login</NuxtLink>
         <NuxtLink to="/about" class="bg-gray-300 hover:bg-gray-400 text-black py-2 px-4 rounded-lg">Learn More</NuxtLink>
       </div>
-    </div>
-  </div>
-</template>
 
-<script>
-
-</script>
-=======
-
-   <div class="max-w-6xl mx-auto bg-sky-100 p-4 rounded-xl">
-    <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
 
   <!-- Header Inputs -->
     <div class="grid grid-cols-4 gap-4">
@@ -166,4 +155,9 @@ import { toValue } from 'vue';
   </div>
 
 </template>
->>>>>>> Stashed changes
+
+<script>
+
+</script>
+
+  
