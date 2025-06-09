71e60dbf14b157286e667269e6f7e83e2598f3be
sajin3317 <vicentesajin@gmail.com>
2025-06-09 21:10:21 +0800

    Update::Transactions

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -6,7 +6,10 @@ import { toValue } from 'vue';
 <template>
 
 <div class ="p-4">
-  <NuxtLink to="/products" class="bg-gray-300 hover:bg-gray-400 text-black py-2 px-4 rounded-lg m-5">GO TO ADMIN for Products</NuxtLink>
+  <NuxtLink to="/products" class="bg-gray-300 hover:bg-gray-400 text-black py-2 px-4 rounded-lg m-5">ADMIN for Products</NuxtLink>
+<NuxtLink to="/transactions" class="bg-gray-300 hover:bg-gray-400 text-black py-2 px-4 rounded-lg m-5">ADMIN for Transactions</NuxtLink>
+
+
 <h1 class="text-5xl font-bold mb-4">Dashboard</h1>
 
 
