01c36b5e9037b740316d8874f9653e3db94e7278
sajin3317 <vicentesajin@gmail.com>
2025-12-08 11:21:01 +0800

    Update3

--- electron/main.js
+++ electron/main.js
@@ -27,7 +27,7 @@ function initDB() {
     fs.mkdirSync(dbDir, { recursive: true })
   }
 
-  const dbPath = path.join(dbDir, 'DBTrial.db')
+  const dbPath = path.join(dbDir, 'DBTrial2.db')
   const db = new Database(dbPath)
 
   console.log('🧭 Using DB at:', dbPath)
