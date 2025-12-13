8767a0ccf8e846c07526f58b2e8c3890e6b286b8
LeaGrailP <leagrailpanganiban@gmail.com>
2025-12-13 12:08:02 +0800

    Revert "Update::Ui"
    This reverts commit 9b4250c4709e6f64977ea9ab399af85242436f29.

---
+++ .output/public/_nuxt/CX5xswon.js
@@ -0,0 +1,5 @@
+<<<<<<<< Updated upstream:.output/public/_nuxt/CX5xswon.js
+import{G as t,H as a}from"./URJV-2WR.js";import{u}from"./CDXjN6Wu.js";const s=t(e=>{const r=u("user");if(!r.value&&e.path!=="/login"&&e.path!=="/register")return a("/index");if(e.meta.requiresAdmin&&r.value?.role!=="admin")return a("/unauthorized");if(e.meta.requiresStaff&&r.value?.role!=="cashier")return a("/unauthorized")});export{s as default};
+========
+import{G as t,H as a}from"./D3CRd6su.js";import{u}from"./o0L4mAwA.js";const s=t(e=>{const r=u("user");if(!r.value&&e.path!=="/login"&&e.path!=="/register")return a("/index");if(e.meta.requiresAdmin&&r.value?.role!=="admin")return a("/unauthorized");if(e.meta.requiresStaff&&r.value?.role!=="cashier")return a("/unauthorized")});export{s as default};
+>>>>>>>> Stashed changes:.output/public/_nuxt/DpdV8wDH.js
